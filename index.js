49f44552cf8e4b25b137dfaf138779c4b86983d6
Rafia Khandaker <rafiakhandaker@gmail.com>
2022-07-22 23:54:46 -0400

    commit tree 48f151566556e7d024b3b517a2b0f82d0468a1f5

--- index.js
+++ index.js
@@ -1,1 +1,41 @@
 // Code your solutions in this file
+/*for (let age=30; age<40; age++){
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    //debugger;
+}
+*/
+
+/*const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+    for (let i=0; i<gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+        //debugger;
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+*/
+
+//for loop 
+function writeCards(name, event) {
+    let messages = [];
+    for (let i=0; i<name.length; i++) {
+        messages.push(`Thank you, ${name[i]}, for the wonderful ${event} gift!`);
+        //debugger;
+    }
+    return messages;
+}
+writeCards(["Guadalupe", "Ollie", "Aki"], "surprise");
+
+
+//while loop
+let n = 4;
+
+function countDown(n){
+    let i = 0;
+    while (i <= n) {
+        console.log(n--);
+    } 
+}
+countDown(10);
